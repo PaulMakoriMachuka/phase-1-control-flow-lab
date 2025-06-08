552189daa0858eab525cc2d8e0f16508248e5159
PaulMakoriMachuka <paulmachukamakori@gmail.com>
2025-06-08 10:54:30 +0300

    my first

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  const freeRideLimit = 400;
+  const twentyDollarLimit = 2000;
+  const maxAllowedDistance = 2500;
+
+  if (feet <= freeRideLimit) {
+    return "This one is on me!";
+  } else if (feet > freeRideLimit && feet <= twentyDollarLimit) {
+    return "That will be twenty bucks.";
+  } else if (feet > twentyDollarLimit && feet <= maxAllowedDistance) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const allowedCity = "NYC";
+  return city === allowedCity ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  const generous = "generous";
+  const notGenerous = "not as generous";
+
+  switch (tip) {
+    case generous:
+      return "Thank you so much.";
+    case notGenerous:
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
